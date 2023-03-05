38496e62059c47ff8b4243e4d169a5f8a18d5c04
MaxRyazan <no4ta87@mail.ru>
2023-03-05 13:17:57 +0300

    1.2.1 some visual fix

--- frontend/src/components/AppAllPosts.vue
+++ frontend/src/components/AppAllPosts.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="all_posts_wrapper" v-for="post in store.state.allPosts" :key="post.id">
-        <div class="all_posts_wrapper_item">
+    <div class="all_posts_wrapper">
+        <div class="all_posts_wrapper_item" v-for="post in store.state.allPosts" :key="post.id">
             <div class="all_posts_wrapper_item-content">{{post.title}}</div>
             <div class="all_posts_wrapper_item-content">{{post.tags}}</div>
             <div class="all_posts_wrapper_item-content">{{post.subject}}</div>
@@ -11,20 +11,46 @@
 <script setup>
 import store from "@/store/store";</script>
 
-<style scoped>
-.all_posts_wrapper{
-    margin-top: 50px;
+<style scoped lang="scss">
+.all_posts_wrapper {
+  margin-top: 50px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(280px, 450px));
+  grid-auto-rows: 300px;
+  gap: 20px;
+}
+
+.all_posts_wrapper_item {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  box-shadow: 3px 4px 4px 3px rgb(221, 221, 221);
+  border-radius: 20px;
+  padding: 40px;
+  overflow: hidden;
+  position: relative;
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0;
     width: 100%;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 10px;
+    height: 40px;
+    background: linear-gradient(180deg, transparent, white 50%);
+  }
+  &:hover {
+    box-shadow: 3px 4px 4px 3px rgb(67, 128, 128);
+    cursor: pointer;
+  }
 }
-.all_posts_wrapper_item{
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-}
-.all_posts_wrapper_item-content{
-    margin-top: 10px;
+
+.all_posts_wrapper_item-content {
+  margin-top: 10px;
+  display: -webkit-box;
+  -webkit-line-clamp: 9;
+  -webkit-box-orient: vertical;
+  line-height: 120%;
+
 }
 </style>
--- frontend/src/logic/actions.ts
+++ frontend/src/logic/actions.ts
@@ -1,23 +1,26 @@
 // @ts-ignore
 import store from "@/store/store.js";
 import {Mutations} from "@/logic/mutations";
+// @ts-ignore
+import {baseURL} from '@/env'
 
 export class Actions {
     async sendNewPostData(){
-        await fetch('http://localhost:3000/posts/new', {
+        const newPost = { title: store.state.title, tags: store.state.tags, subject: store.state.subject }
+        await fetch(baseURL + '/posts/new', {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({title: store.state.title, tags: store.state.tags, subject: store.state.subject})
+            body: JSON.stringify(newPost)
         })
+        store.state.allPosts.push(newPost)
         const mutations = new Mutations();
         mutations.clearInputFields()
     }
 
     async getAllPosts(){
-        const posts = await fetch('http://localhost:3000/posts')
+        const posts = await fetch(baseURL + '/posts')
         store.state.allPosts = await posts.json()
-        console.log(store.state.allPosts)
     }
 }
